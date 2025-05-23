061b04d403603cff20ada287751b13c6cb636b7f
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2025-05-24 00:41:39 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+# Read the README file for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fck",
+    version="0.1",
+    author="AKM Korishee Apurbo",
+    author_email="bandinvisible8@gmail.com",
+    description="FCK – The Ultimate Brute Forcer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IMApurbo/fck",  
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "rich>=13.3.5",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "fck = fck.fck:main",
+        ],
+    },
+    license="MIT",  # Explicitly set the license instead of `license-file`
+    include_package_data=True,  # Ensure additional files like LICENSE are included
+)
