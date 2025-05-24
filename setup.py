f6dfaa133e802e9d1f742892204c12cffe498109
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2025-05-24 09:37:48 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="fck",
+    name="fckr",
     version="0.1",
     author="AKM Korishee Apurbo",
     author_email="bandinvisible8@gmail.com",
