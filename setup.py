7518b81360052afc03b9a31c36f94ef63452a247
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2025-05-24 10:25:59 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fckr",
-    version="0.3",
+    version="0.4",
     author="AKM Korishee Apurbo",
     author_email="bandinvisible8@gmail.com",
     description="FCKR – The Ultimate Brute Forcer",
